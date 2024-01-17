226efd98f1375bd256ba52cd0bbc787e825d1ff5
Oskar Pieniak <pieniakoskar44@gmail.com>
2024-01-17 17:24:25 +0100

    sanity schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+import account from "./accounts"
+import booking from "./booking"
+import hotelRoom from "./hotelRoom"
+import review from "./review"
+import user from "./user"
+
+export const schemaTypes = [user, account, booking, hotelRoom, review]
+
