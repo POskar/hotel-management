cd387a86152c3fd80b6ebaa5afee8be629d38931
Oskar Pieniak <pieniakoskar44@gmail.com>
2024-02-02 19:32:26 +0100

    room details

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['lh3.googleusercontent.com', 'cdn.pixabay.com', 'pixabay.com', '*.pixabay.com'],
-    }
-}
+        domains: [
+            'lh3.googleusercontent.com',
+            'cdn.pixabay.com',
+            'pixabay.com',
+            '*.pixabay.com',
+            'images.unsplash.com',
+            '*.images.unsplash.com',
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
