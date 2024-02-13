bd13ea0f01ca70e7fc10782912fb71dab0161467
Oskar Pieniak <pieniakoskar44@gmail.com>
2024-02-13 11:31:07 +0100

    user ratings are displayed on the room details page

--- src/app/(web)/rooms/[slug]/page.tsx
+++ src/app/(web)/rooms/[slug]/page.tsx
@@ -13,6 +13,7 @@ import BookRoomCta from '@/components/BookRoomCta/BookRoomCta'
 import toast from 'react-hot-toast'
 import axios from 'axios'
 import { getStripe } from '@/libs/stripe'
+import RoomReview from '@/components/RoomReview/RoomReview'
 
 const RoomDetails = (props: { params: { slug: string } }) => {
   const {
@@ -160,7 +161,7 @@ const RoomDetails = (props: { params: { slug: string } }) => {
                   <p className='md:text-lg font-semibold'>Customer Reviews</p>
                 </div>
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-                  {/* <RoomReview roomId={room._id} /> */}
+                  <RoomReview roomId={room._id} />
                 </div>
               </div>
             </div>
